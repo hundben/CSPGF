f5f746af78ee3f5eb21721d5e1be9afcf5f14e43
Erik Bergstrom <erktheorc@gmail.com>
2011-09-21 12:25:08 +0200

    Reduced cyclomatic complexity. Still bad.

--- CSPGF/CSPGF/parser/ActiveItem.cs
+++ CSPGF/CSPGF/parser/ActiveItem.cs
@@ -98,11 +98,10 @@ namespace CSPGF.Parse
         {
             if (this.Position < this.Function.Sequences[this.Constituent].Length) 
             {
-                Symbol sym = this.Function.Sequences[this.Constituent][this.Position];
-                return sym;
+                return this.Function.Sequences[this.Constituent][this.Position];
             }
 
-            return null;    // this might be dangerous
+            return null;
         }
 
         /// <summary>
@@ -116,24 +115,30 @@ namespace CSPGF.Parse
                 this.Category == ai.Category &&
                 this.Function == ai.Function &&
                 this.Constituent == ai.Constituent &&
-                this.Position == ai.Position) 
+                this.Position == ai.Position &&
+                this.Domain.Length == ai.Domain.Length) 
             {
-                // Since there is no deep method in c# that we know of, use a crappy forloop
-                if (this.Domain.Length == ai.Domain.Length) 
-                {
-                    for (int i = 0; i < this.Domain.Length; i++) 
-                    {
-                        if (this.Domain[i] != ai.Domain[i])
-                        {
-                            return false;
-                        }
-                    }
-
-                    return true;
-                }
+                return DeepCheck(ai);
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// Since there is no deep method in c# that we know of we have to use for-loops.
+        /// </summary>
+        /// <param name="ai">The active item.</param>
+        /// <returns>True if equal.</returns>
+        private bool DeepCheck(ActiveItem ai)
+        {
+            for (int i = 0; i < this.Domain.Length; i++)
+            {
+                if (this.Domain[i] != ai.Domain[i])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         /// <summary>
