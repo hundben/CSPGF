de1da9409c445a6178b58887045dc7009d8c0a64
Erik Bergstrom <erktheorc@gmail.com>
2011-10-19 12:01:42 +0200

    Clean up...

--- CSPGF/CSPGF/parser/TreeConverter.cs
+++ CSPGF/CSPGF/parser/TreeConverter.cs
@@ -116,7 +116,6 @@ namespace CSPGF.Parse
                     trees.Add(this.C2a(tr, vars));
                 }
 
-                // TODO: Check if it works :D
                 return trees.Aggregate<CSPGF.Trees.Absyn.Tree>(this.MkEApp);
             }
             else if (t is Literal) 
@@ -161,27 +160,3 @@ namespace CSPGF.Parse
         }
     }
 }
-
-// object TreeConverter {
-//  def intermediate2abstract (t : Tree): ETree = {
-//    def c2a(t : Tree, vars:List[String]): ETree = t match {
-//      // Here we have to convert sugarized λ-abstraction (λx,y,z → ...)
-//      // to canonical ones (λx→λy→λz→...)
-//      case Lambda(lvars, body) =>
-//        lvars.foldRight(c2a(body, lvars.map(_._2).reverse ++ vars))(mkELambda)
-//      // Here variables are index by name but abstract
-//      // syntax uses de Bruijn indices
-//      case Variable(x) => new EVariable(vars.indexOf(x))
-//      // Here we have to desugarized applicaton :
-//      // f a b c becomes (((f a) b) c)
-//      case Application(fun,args) =>
-//        args.map(c2a(_, vars)).foldLeft(new EFunction(fun):ETree)(mkEApp)
-//      case Literal(value) => new ELiteral(new StringLiteral(value))
-//      case MetaVariable(id) => new EMetaVariable(id)
-//    }
-//    def mkEApp(f : ETree, x : ETree):ETree = new EApplication(f,x)
-//    def mkELambda(v :(Boolean, String) , body:ETree ):ETree = v match {
-//      case (bindType, name) => new ELambda(name, body)
-//    }
-//    c2a(t, Nil)
-//  }
