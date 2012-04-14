7766ec7b6034f9a6f9b8b0091dad24ba5f7ad4a9
Erik Bergstrom <erktheorc@gmail.com>
2012-04-14 17:31:53 +0200

    * Fixed some comments.

--- CSPGF/CSPGF/parser/ParseTrie.cs
+++ CSPGF/CSPGF/parser/ParseTrie.cs
@@ -1,6 +1,30 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="ParseTrie2.cs" company="">
-// TODO: Update copyright text.
+// <copyright file="ParseTrie.cs" company="None">
+//  Copyright (c) 2011, Christian Ståhlfors (christian.stahlfors@gmail.com), 
+//   Erik Bergström (erktheorc@gmail.com) 
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of the &lt;organization&gt; nor the
+//     names of its contributors may be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS &quot;AS IS&quot; AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL &lt;COPYRIGHT HOLDER&gt; BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // </copyright>
 // -----------------------------------------------------------------------
 
@@ -16,23 +40,35 @@ namespace CSPGF.Parse
     /// </summary>
     internal class ParseTrie
     {
-
+        /// <summary>
+        /// The agenda connected to this node.
+        /// </summary>
         private Stack<ActiveItem> value = new Stack<ActiveItem>();
 
-        private Dictionary<string, ParseTrie> childs = new Dictionary<string,ParseTrie>();
+        /// <summary>
+        /// List of subtries.
+        /// </summary>
+        private Dictionary<string, ParseTrie> childs = new Dictionary<string, ParseTrie>();
 
+        /// <summary>
+        /// Initializes a new instance of the ParseTrie class.
+        /// </summary>
         public ParseTrie()
         {
-
         }
 
+        /// <summary>
+        /// Add a key or a list of keys to the parse trie with the corresponding agenda.
+        /// </summary>
+        /// <param name="keys">A list of strings, one subtrie for each one.</param>
+        /// <param name="value">Value attached to the subtrie of the last key.</param>
         public void Add(List<string> keys, Stack<ActiveItem> value)
         {
             ParseTrie newTrie = this;
 
             foreach (string k in keys)
             {
-                if (!childs.ContainsKey(k))
+                if (!this.childs.ContainsKey(k))
                 {
                     ParseTrie tt = new ParseTrie();
                     newTrie.childs[k] = tt;
@@ -48,6 +84,11 @@ namespace CSPGF.Parse
             newTrie.value = value;
         }
 
+        /// <summary>
+        /// Searches the current node for the agenda of a subtrie with the corresponding key.
+        /// </summary>
+        /// <param name="key">The key to search for.</param>
+        /// <returns>The agenda.</returns>
         public Stack<ActiveItem> Lookup(string key)
         {
             ParseTrie trie;
@@ -64,12 +105,18 @@ namespace CSPGF.Parse
             return null;
         }
 
+        /// <summary>
+        /// Same as Lookup but searches down the trie.
+        /// </summary>
+        /// <param name="keys">The list of keys.</param>
+        /// <returns>The agenda.</returns>
         public Stack<ActiveItem> Lookup(List<string> keys)
         {
             if (keys.Count == 0)
             {
                 return this.value;
             }
+
             Stack<ActiveItem> items = new Stack<ActiveItem>();
             ParseTrie currentTrie = this;
             foreach (string k in keys)
@@ -84,10 +131,14 @@ namespace CSPGF.Parse
                 }
             }
 
-            return currentTrie.value ;
+            return currentTrie.value;
         }
 
-
+        /// <summary>
+        /// Returns the subtrie that corresponds to the key.
+        /// </summary>
+        /// <param name="key">The key we want to search for.</param>
+        /// <returns>The subtrie.</returns>
         public ParseTrie GetSubTrie(string key)
         {
             ParseTrie trie;
@@ -95,9 +146,15 @@ namespace CSPGF.Parse
             {
                 return trie;
             }
+
             return null;
         }
 
+        /// <summary>
+        /// Same as with one key but searches down the trie.
+        /// </summary>
+        /// <param name="keys">A list of keys.</param>
+        /// <returns>The subtrie.</returns>
         public ParseTrie GetSubTrie(List<string> keys)
         {
             ParseTrie trie = this;
@@ -106,21 +163,30 @@ namespace CSPGF.Parse
                 // TODO optimize to use TryGet
                 if (trie.childs.ContainsKey(k))
                 {
-                    trie = childs[k];
+                    trie = this.childs[k];
                 }
                 else
                 {
                     return null;
                 }
             }
+
             return trie;
         }
 
+        /// <summary>
+        /// Returns the predictions of the current node.
+        /// </summary>
+        /// <returns>A list of predictions (tokens).</returns>
         public List<string> Predict()
         {
             return this.childs.Keys.ToList<string>();
         }
 
+        /// <summary>
+        /// Not implemented yet...
+        /// </summary>
+        /// <param name="token">The current token.</param>
         public void ResetChild(string token)
         {
         }
