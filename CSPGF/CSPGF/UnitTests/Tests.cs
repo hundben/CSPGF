febca0ee2f4362dcae8fe198d2b80c79b407faa1
Erik Bergstrom <erktheorc@gmail.com>
2016-03-16 11:38:09 +0100

    Migrated to xUnit. Moved example files and created a new example folder.

---
+++ CSPGF/CSPGF/UnitTests/Tests.cs
@@ -0,0 +1,60 @@
+﻿
+using Xunit;
+
+namespace CSPGF
+{
+    public class Tests
+    {
+        [Fact]
+        public void NormalTest1()
+        {
+            AdvancedTranslator at = new AdvancedTranslator("../../PGF examples/Phrasebook.pgf");
+            at.SetInputLanguage("PhrasebookEng");
+            at.Scan("this");
+            at.Scan("wine");
+            at.Scan("is");
+            at.Scan("delicious");
+            at.Scan(".");
+            at.SetOutputLanguage("PhrasebookEng");
+            string check = at.Translate();
+            Assert.Equal(true, check.Equals("this wine is delicious ."));
+        }
+
+        /// <summary>
+        /// Simple test to test literals.
+        /// </summary>
+        [Fact]
+        public void LiteralTest1()
+        {
+            AdvancedTranslator at = new AdvancedTranslator("../../PGF examples/MiniLit.pgf");
+            at.SetInputLanguage("MiniLitCnc");
+            at.SetOutputLanguage("MiniLitCnc");
+            at.Scan("flt");
+            at.Scan("(");
+            at.Scan("1.2");
+            at.Scan(")");
+            string check = at.Translate();
+
+            // TODO write this one
+            Assert.Equal(false, true);
+        }
+
+        /// <summary>
+        /// A simple translation test from English to Swedish.
+        /// </summary>
+        [Fact]
+        public void TranslationTest1()
+        {
+            AdvancedTranslator at = new AdvancedTranslator("../../PGF examples/Phrasebook.pgf");
+            at.SetInputLanguage("PhrasebookEng");
+            at.Scan("this");
+            at.Scan("wine");
+            at.Scan("is");
+            at.Scan("delicious");
+            at.Scan(".");
+            at.SetOutputLanguage("PhrasebookSwe");
+            string check = at.Translate();
+            Assert.Equal(true, check.Equals("det här vinet är läckert ."));
+        }
+    }
+}
